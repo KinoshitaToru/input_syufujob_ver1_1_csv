a84576928f48b4fd07284db8c1eaa9cbaa65197e
木下 暢 <kinoshita-t@dym.local>
2024-05-14 15:33:46 +0900

    add logic to remove additional emoji

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -132,6 +132,7 @@ def lambda_handler(event, context):
         df = df.replace('\u2730', '', regex=True) #✰
         df = df.replace('\u0f1a', '', regex=True) #。
         df = df.replace('\u2002', '', regex=True)
+        df = df.replace('\ufe0f', '', regex=True)
 
         # DataFrame を CSV ファイルに出力
         df.to_csv(input_file_path, sep=',', encoding='CP932', index=False)
